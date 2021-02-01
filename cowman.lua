ee51c43fe72619592bcfcadee80f3b7617ad9a40
Melvin Bentinger <melvinbentinger2@gmail.com>
2021-02-01 22:20:53 +0100

    COW MAN IS GONE!

--- cowman.lua
+++ cowman.lua
@@ -1,11 +1,43 @@
 local cowman = {}
-function cowman:load()
-    Texture = love.graphics.newImage("Assets/cowman.png")
-    Quad = love.graphics.newQuad(0,0,16,16,16,16)
+
+function cowman:Load()
+    cowman.Speed = 1
+    cowman.JumpHeight = 4
+    cowman.Gravity = 0.2
+    cowman.OnGround = false
+
+    cowman.Position = {["X"] = 64, ["Y"] = 64}
+    cowman.Velocity = {["X"] = 0, ["Y"] = 0}
+    cowman.Texture = love.graphics.newImage("Assets/cowman.png")
+    cowman.Quad = love.graphics.newQuad(0,0,16,16,cowman.Texture:getWidth(),cowman.Texture:getHeight())
+end
+
+function cowman:Update()
+    cowman.Velocity.X = 0;
+    if love.keyboard.isDown("a") then
+        cowman.Velocity.X = cowman.Velocity.X - cowman.Speed
+    end
+    if love.keyboard.isDown("d") then
+        cowman.Velocity.X = cowman.Velocity.X + cowman.Speed
+    end
+    if love.keyboard.isDown("w") and cowman.OnGround == true then
+        cowman.Velocity.Y = cowman.Velocity.Y - cowman.JumpHeight
+    end
+    cowman.Velocity.Y = cowman.Velocity.Y + cowman.Gravity
+
+    cowman.Position.X = cowman.Position.X + cowman.Velocity.X
+    cowman.Position.Y = cowman.Position.Y + cowman.Velocity.Y
+
+    cowman.OnGround = false
+    if cowman.Position.Y > 200 then
+        cowman.Position.Y = 200
+        cowman.Velocity.Y = 0
+        cowman.OnGround = true
+    end
 end
 
 function cowman:Draw()
-    love.graphics.draw(Texture, Quad, 128, 128)
+    love.graphics.draw(cowman.Texture, cowman.Quad, cowman.Position.X, cowman.Position.Y)
 end
-return cowman
 
+return cowman
--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
 cowman = require "cowman"
+
 function love.load()
-    cowman.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    cowman.Load()
+end
+
+function love.update(dt)
+    cowman.Update()
 end
 
 function love.draw ()
+    love.graphics.scale(2,2)
     cowman.Draw()
 end
